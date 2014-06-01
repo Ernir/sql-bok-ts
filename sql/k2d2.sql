206ed198c31dc2dc892596105bc5c599867ce181
Ernir <e.ernir@gmail.com>
2014-06-01 18:14:53 +0000

    Sýnidæmum fyrir INSERT og SELECT bætt við

---
+++ sql/k2d2.sql
@@ -0,0 +1,16 @@
+INSERT INTO 
+  Starfsmenn(nafn, starfsheiti, netfang) 
+VALUES
+  ("Bjargey G. Gísladóttir", "Skólastjóri","bgg@tskoli.is"),
+  ("Eiríkur Benediktsson", "Kennari","ebe@tskoli.is"),
+  ("Eiríkur Ernir Þorsteinsson", "Kennari","eet@tskoli.is"),
+  ("Emil Gautur Emilsson", "Kennari","ege@tskoli.is"),
+  ("Geir Sigurðsson", "Kennari","ges@tskoli.is"),
+  ("Guðmundur Jón Guðjónsson", "Kennari","gjg@tskoli.is"),
+  ("Guðrún Randalín Lárusdóttir", "Kennari","grl@tskoli.is"),
+  ("Gunnar Sigurðsson", "Kennari","gus@tskoli.is"),
+  ("Hallur Ó. Karlsson", "Kennari","hal@tskoli.is"),
+  ("Konráð Guðmundsson", "Kennari","kng@tskoli.is"),
+  ("Sigurður R. Ragnarsson", "Kennari","srr@tskoli.is"),
+  ("Snorri Emilsson", "Kennari","sem@tskoli.is"),
+  ("Þórarinn J. Kristjánsson", "Kennari","tjk@tskoli.is");
---
+++ sql/k2d3.sql
@@ -0,0 +1,3 @@
+SELECT nafn
+FROM Starfsmenn
+WHERE netfang = "kng@tskoli.is";
