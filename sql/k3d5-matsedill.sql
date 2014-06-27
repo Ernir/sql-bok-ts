5f185df805e9b7cd267fc1f08fd2e54fb8e0c6fb
Ernir <e.ernir@gmail.com>
2014-06-27 12:23:17 +0000

    INSERT skipunum bætt við sýnidæmi

--- sql/k3d5-matsedill.sql
+++ sql/k3d5-matsedill.sql
@@ -1,7 +1,19 @@
 CREATE TABLE Hamborgarar
 (
-  numer INTEGER PRIMARY KEY NOT NULL,
+  numer INTEGER,
   nafn VARCHAR(50),
   verd INTEGER,
   lysing VARCHAR(255)
 );
+
+INSERT INTO 
+  Hamborgarar(numer, nafn, verd, lysing)
+VALUES
+  (1, 'Hamborgari', 690, 
+    '120g nautakjöt, kál, tómatar'),
+  (2, 'Ostborgari', 750, 
+    '120g nautakjöt, ostur, kál, tómatar'),
+  (3, 'Beikonborgari', 890, 
+    '120g nautakjöt, ostur, beikon, kál, tómatar'),
+  (4, 'Sá stóri', 1180, 
+    '200g nautakjöt, ostur, beikon, kál, tómatar');
