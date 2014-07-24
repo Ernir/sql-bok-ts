cd9d54ebc53cdb7b7ca3aeeaaa9684f0badf6507
Ernir <e.ernir@gmail.com>
2014-07-24 14:30:54 +0000

    Sýnidæmi um tvöfalt INNER JOIN bætt við

---
+++ sql/k6d4-tvofalt-join.sql
@@ -0,0 +1,6 @@
+SELECT Hopar.nafnHops, Afangar.audkenni, Starfsmenn.nafn
+FROM Afangar
+INNER JOIN Hopar
+ON Afangar.numer = Hopar.afangaNumer
+INNER JOIN Starfsmenn
+ON Hopar.kennaraNumer = Starfsmenn.numer; 
